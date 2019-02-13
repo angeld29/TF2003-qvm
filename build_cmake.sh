c9a075543afbbdf56a4163cdf0c760edf30217dc
angeld <roman.andriyanov@corp.mail.ru>
2019-02-13 11:28:10 +0000

    cmake

---
+++ build_cmake.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+rm -rf _cmake
+mkdir _cmake
+for name in windows-x64 windows-x86; do
+#for name in linux-amd64 linux-armhf linux-i686 windows-x64 windows-x86; do
+    mkdir _cmake/$name
+    cd _cmake/$name
+    cmake -DCMAKE_TOOLCHAIN_FILE=../../tools/cross-cmake/$name.cmake -G Ninja ../..
+    cmake --build .
+    cd ../..
+done;
+
+
